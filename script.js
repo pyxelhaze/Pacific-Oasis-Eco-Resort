d5adf284c6d034af99dfcdf12d0e77c1f4a74c36
pyxelhaze <110282836+pyxelhaze@users.noreply.github.com>
2023-12-08 09:39:57 +0100

    added welcometext button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+let slideIndex = 0;
+
 const toggleForm = () => {
     const form = document.querySelector(".myForm");
     if (form.style.display === "none" || form.style.display === "") {
@@ -33,44 +35,10 @@ document.querySelectorAll(".minus").forEach(function (button) {
     });
 });
 
-let slideIndex = 1;
-
-//  Lightbox
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
-
-let currentSlideModal = 1;
-showSlidesModal(currentSlideModal);
-
-function plusSlidesModal(n) {
-    showSlidesModal(currentSlideModal += n);
-}
-
-function currentSlide(n) {
-    showSlidesModal(currentSlideModal = n);
-}
-
-function showSlidesModal(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {
-        currentSlideModal = 1;
-    }
-    if (n < 1) {
-        currentSlideModal = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[currentSlideModal - 1].style.display = "block";
-}
 
 //Slideshow
+let carouselTimeout;
+
 const carousel = () => {
     let i;
     let x = document.getElementsByClassName("fade");
@@ -78,13 +46,60 @@ const carousel = () => {
         x[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > x.length) {
-        slideIndex = 1;
-    }
+    if (slideIndex > x.length) { slideIndex = 1 }
     x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 4000);
+    carouselTimeout = setTimeout(carousel, 4000);
 }
 carousel();
+
+// carousel stop
+const stopCarousel = () => {
+    clearTimeout(carouselTimeout);
+}
+
+// fullscreen img
+const images = document.querySelectorAll('img');
+
+images.forEach(image => image.addEventListener('click', (e) => {
+    if (!document.fullscreenElement) {
+        image.requestFullscreen().then(() => {
+            stopCarousel();
+        }).catch(error => {
+            console.error('Fehler beim Aktivieren des Vollbildmodus:', error);
+        });
+    } else if (document.fullscreenElement && document.exitFullscreen) {
+        document.exitFullscreen().then(() => {
+
+            carousel();
+        }).catch(error => {
+            console.error('Fehler beim Verlassen des Vollbildmodus:', error);
+        });
+    }
+}));
+
+document.addEventListener('fullscreenchange', (e) => {
+    if (document.fullscreenElement) {
+        stopCarousel();
+    } else {
+        carousel();
+    }
+});
+
+//hidden welcome text
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("toggleButton");
+    const hiddenText = document.getElementById("hiddenText");
+
+    toggleButton.addEventListener("click", () => {
+        if (hiddenText.style.display === "none" || hiddenText.style.display === "") {
+            hiddenText.style.display = "block";
+            toggleButton.innerText = "Read Less";
+        } else {
+            hiddenText.style.display = "none";
+            toggleButton.innerText = "Read More";
+        }
+    });
+});
 
 
 
